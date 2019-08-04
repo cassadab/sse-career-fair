617325ef09da04115e66761df21d63572c5b6470
Brady Cassada <bradycassada@gmail.com>
2019-08-03 20:15:50 -0500

    Imported auto-gen front end stuff and have the index page mostly up.

--- career_fair/views.py
+++ career_fair/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-# Create your views here.
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the career fair index.")
-
+    return render(request, 'index.html')
